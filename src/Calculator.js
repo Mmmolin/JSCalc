fc37053c297c5cc190daefaa572832efedd25d8c
unknown <mmmolin78@gmail.com>
2018-09-22 13:50:13 +0200

    first commit

---
+++ src/Calculator.js
@@ -0,0 +1,85 @@
+let result = [];
+loadEventListeners();
+
+
+/* EventListeners */
+function loadEventListeners() {
+    number();
+    operator();
+};
+
+function number() {
+    let numbers = document.querySelectorAll('.number');
+    numbers.forEach(function(number) {
+        number.addEventListener('click', function() {
+            inputDisplay(number.value);
+        });
+    });
+};
+
+function operator(){
+    let operators = document.querySelectorAll('.operator');
+    operators.forEach(function(operator) {
+        operator.addEventListener('click', function() {
+            if(operator.value == "+") {
+                addition();
+            }
+            else if(operator.value == "-") {
+                subtraction();
+            } else {
+                equals();
+            }
+        });
+    });
+};
+
+/* Not EventListeners */
+
+function inputDisplay(number) {
+    let display = document.querySelector('#display');
+    if(display.textContent == "+" || display.textContent == "-"){
+        display.textContent = "";
+    };
+    display.textContent += number;
+};
+
+function addition() {
+    let display = document.querySelector('#display');
+    result.push(Number(display.textContent));
+    result.push("+");
+    display.textContent = "+"; 
+};
+
+function subtraction() {
+   let display = document.querySelector('#display');
+   result.push(Number(display.textContent));
+   result.push("-");
+   display.textContent = "-";
+}
+
+function equals() {
+    if(result[result.length - 1] != "+" || 
+    result[result.length -1] != "-") {
+        let display = document.querySelector('#display');
+        result.push(Number(display.textContent));
+    };
+    let sum = 0;
+    for(i = 0; i < result.length; i++) {
+        if(i == 0) {
+            sum = result[i];
+        }
+        else if(result[i] == "+") {
+            sum += result[i + 1];
+        }
+        else if(result[i] == "-") {
+            sum -= result[i + 1];
+        }
+    }
+    let display = document.querySelector('#display');
+    display.textContent = sum;
+};
+
+
+function clear() {
+     
+}
